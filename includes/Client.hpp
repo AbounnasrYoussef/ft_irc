d124ed5315e2011f3e35ac71fefb8128cf675871
Naoufal Bougrine <nbougrin@e1r10p12.1337.ma>
2025-12-25 12:28:48 +0100

    add files for project structure

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -5,9 +5,37 @@
 // void removeClient(struct pollfd fds[], Client* clients[], int& num_fds, int index);
 
 class Client {
-    public:
-        Client(){}
-        ~Client(){}
-
+	private:
+		int _fd;                    // Client file descriptor
+		std::string _nickname;      // Client nickname
+		std::string _username;      // Client username
+		std::string _realname;      // Real name
+		std::string _buffer;        // Message buffer (for partial messages)
+		bool _passOk;               // Password authenticated?
+		bool _registered;           // Fully registered? (PASS + NICK + USER)
+		
+	public:
+		Client(int fd);
+		~Client();
+		
+		// Getters
+		int getFd() const;
+		std::string getNickname() const;
+		std::string getUsername() const;
+		bool isRegistered() const;
+		bool isPassOk() const;
+		std::string getBuffer() const;
+		
+		// Setters
+		void setNickname(std::string nick);
+		void setUsername(std::string user);
+		void setRealname(std::string real);
+		void setPassOk(bool ok);
+		void setRegistered(bool reg);
+		
+		// Buffer management
+		void appendBuffer(std::string data);
+		void clearBuffer();
 };
-#endif // CLIENT_HPP
+
+#endif
--- includes/Server.hpp
+++ includes/Server.hpp
@@ -2,18 +2,47 @@
 #define SERVER_HPP
 
 #include <iostream>
+# define MAX_CLIENTS 10
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <poll.h>
+#include <stdlib.h>
+
+class Client;
 
 class Server {
 	private:
-		int _port;
-		std::string _password;
+		int _serverFd;                    // Server socket fd
+		int _port;                        // Port number
+		std::string _password;            // Server password
+		struct pollfd _fds[MAX_CLIENTS];  // Poll array
+		Client* _clients[MAX_CLIENTS];    // Array of client pointers
+		int _numFds;                      // Number of active fds
+	
 	public:
 		Server();
-		Server(int port, const std::string& password);
 		Server(const Server& other);
 		Server& operator=(const Server& other);
+		Server(int port, std::string password);
 		~Server();
-		// getters/setters...
+	
+	// Core server functions
+	void start();                     // Main server loop
+	void setupSocket();               // socket() + bind() + listen()
+	void acceptNewClient();           // accept() new connection
+	void handleClientData(int index); // Process client messages
+	void removeClient(int index);     // Disconnect and cleanup
+	
+	// Message handling
+	void processCommand(Client* client, std::string message);
+	void broadcastToChannel(std::string channelName, std::string message, Client* sender);
+	
+	// Utilities
+	Client* getClientByNick(std::string nickname);
+	bool isNicknameTaken(std::string nickname);
 };
+
 
 #endif
